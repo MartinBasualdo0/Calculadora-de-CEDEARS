918830795febda0b179af7fff68323729c034a38
keynesisnotdead <70729951+MartinBasualdo0@users.noreply.github.com>
2024-01-23 14:31:59 -0300

    app running

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import dash
 from dash import dcc, html, State
 from dash.dependencies import Output, Input
@@ -23,55 +24,59 @@ app.layout = html.Div([
     html.Div([
         html.H1('Calculadora de precios para CEDEARS', className='display-4 mb-3')
     ], className='container'),
-    html.Div([
-        dcc.Input(
-            id='estimate-input',
-            type='text',
-            value=default_ticker,
-            className='mb-3',
-            placeholder="Escribir ticker...",
-            debounce=True,
-        ),
-        dcc.Store(
-            id='input-store',
-            storage_type='session'
-        ),
-        html.Button(
-            'Add Ticker',
-            id='add-ticker-button',
-            className='btn btn-primary mb-3'
-        ),
-        dcc.Loading(
-            id="loading",
-            type="circle",
-            children=[html.Div(id='estimate-result', className='lead')]
-        ),
-        dcc.Dropdown(
-            id='ticker-dropdown',
-            options=[{'label': ticker, 'value': ticker} for ticker in CEDEARS_SELECTOS], 
-            value=CEDEARS_SELECTOS,
-            multi=True,
-            className='mb-3'
-        ),
-        dag.AgGrid(id='output-div'),
-        html.Button("Download Excel", id="btn_xlsx", className='btn btn-primary mb-3'),
-        dcc.Download(id="download-dataframe-xlsx"),
-        dcc.Graph(
-            id = 'tc-por-cedear-plot',
-            figure=plot_ccl_por_cedear(df)
-        )
+    dcc.Loading(
+                id="loading-hole",
+                type="circle",
+                children=[
+        html.Div([
+            dcc.Input(
+                id='estimate-input',
+                type='text',
+                value=default_ticker,
+                className='mb-3',
+                placeholder="Escribir ticker...",
+                debounce=True,
+            ),
+            dcc.Store(
+                id='input-store',
+                storage_type='session'
+            ),
+            dcc.Store(
+                id='store', 
+                storage_type='session',
+                # data = {'CEDEARS_SELECTOS': CEDEARS_SELECTOS, 'df': df}
+                data = {'CEDEARS_SELECTOS': CEDEARS_SELECTOS, 'df': df.to_dict()}
+                ),
+            html.Button(
+                'Add Ticker',
+                id='add-ticker-button',
+                className='btn btn-primary mb-3'
+            ),
+            dcc.Loading(
+                id="loading",
+                type="circle",
+                children=[html.Div(id='estimate-result', className='lead')]
+            ),
+            dag.AgGrid(id='output-div'),
+            html.Button("Download Excel", id="btn_xlsx", className='btn btn-primary mb-3'),
+            dcc.Download(id="download-dataframe-xlsx"),
+            dcc.Graph(
+                id = 'tc-por-cedear-plot',
+                figure=plot_ccl_por_cedear(df) #should read from the stored "df"
+            )
+    ])
     ], className='container'),
 ])
 
 @app.callback(
     Output('output-div', 'rowData'),
     Output('output-div', 'columnDefs'),
-    Input('ticker-dropdown', 'value')
+    Input('store', 'data')
 )
-def update_output(ticker):
+def update_output(data):
+    df = pd.DataFrame.from_dict(data['df'])
     columnDefs = [{"headerName": col, "field": col} for col in df.columns]
     rowData = df.to_dict('records')
-    
     return rowData, columnDefs
 
 @app.callback(
@@ -83,43 +88,51 @@ def store_input(value):
     return value
 
 @app.callback(
-    Output('output-div', 'rowData', allow_duplicate=True),
-    Output('output-div', 'columnDefs', allow_duplicate=True),
-    # Output('estimate-input', 'value'),
+    Output('store', 'data'),
     Input('add-ticker-button', 'n_clicks'),
-    State('input-store', 'data'),
-    prevent_initial_call = True
+    Input('input-store', 'data'),
+    prevent_initial_call=True
 )
 def add_ticker(n_clicks, ticker):
-    global CEDEARS_SELECTOS
-    global df
-    if n_clicks > 0 and ticker not in CEDEARS_SELECTOS:
-        CEDEARS_SELECTOS.append(ticker)
-    df = main_table(CEDEARS_SELECTOS) # Run the main_table function with the updated list
-    columnDefs = [{"headerName": col, "field": col} for col in df.columns]
-    rowData = df.to_dict('records')
-    
-    return rowData, columnDefs
+    if n_clicks:
+        if n_clicks > 0 and ticker not in CEDEARS_SELECTOS:
+            CEDEARS_SELECTOS.append(ticker)
+    df = main_table(CEDEARS_SELECTOS)
+    return {'CEDEARS_SELECTOS': CEDEARS_SELECTOS, 'df': df.to_dict()}
 
 @app.callback(
     Output('estimate-result', 'children'),
-    Input('estimate-input', 'value')
+    Input('add-ticker-button', 'n_clicks'),
+    Input('store', 'data'),
+    Input('input-store', 'data'),
 )
-def update_estimate(selected_ticker):
-    selected_ticker = selected_ticker.upper()
+def update_estimate(n_clicks, data, selected_ticker):
+    df = pd.DataFrame.from_dict(data['df'])
     try:
         estimate = calculate_price(selected_ticker, df)
     except Exception as e:
         return "Cedear incorrecto, intente de nuevo"
     return estimate
 
+# @app.callback(
+#     Output("download-dataframe-xlsx", "data"),
+#     Input("btn_xlsx", "n_clicks"),
+#     Input('store', 'data'),
+#     prevent_initial_call=True,
+# )
+# def export_excel(n_clicks, data):
+#     '''Excel'''
+#     df = pd.DataFrame.from_dict(data['df'])
+#     return dcc.send_data_frame(df.to_excel, "calculadora cedears.xlsx") #should read from the stored "df"
+
 @app.callback(
-    Output("download-dataframe-xlsx", "data"),
-    Input("btn_xlsx", "n_clicks"),
-    prevent_initial_call=True,
+    Output('tc-por-cedear-plot', 'figure'),
+    Input('store', 'data')
 )
-def func(n_clicks):
-    return dcc.send_data_frame(df.to_excel, "calculadora cedears.xlsx")
+def update_graph(data):
+    '''Gráfico corregido con los nuevos cedears'''
+    df = pd.DataFrame.from_dict(data['df'])
+    return plot_ccl_por_cedear(df)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- src/estimated_price.py
+++ src/estimated_price.py
@@ -5,5 +5,6 @@ from src.tablas import get_main_table
 locale.setlocale(locale.LC_ALL, 'es_AR.UTF-8')
 
 def calculate_price(ticker:str, df:pd.DataFrame) -> str:
+    ticker = ticker.upper()
     price = df.loc[df['tickers USA'] == ticker, 'Tenés que comprar el cedear a $'].values[0]
     return f"El precio estimado debería ser: {locale.currency(price, grouping=True)}"
